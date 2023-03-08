6c151d07da79d0a2dea6c4e163fcd3aa17c2a8de
HauTC <conghau0112@gmail.com>
2023-03-08 11:25:46 +0700

    push code

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Nhan Babe';
+const giftUrl = 'https://www.facebook.com/phan.nhan.56679015';
+const eventName = 'Happy 8/3';
+const titleCard = 'Happy Women’s Day';
+const contentCard = 'Hi bé! Với tất cả tấm lòng mình, anh xin chúc em ngày càng xinh đẹp "dã man", thông minh "khủng khiếp", và mau ăn chóng lớn nhá hihi!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'image1.jpeg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
